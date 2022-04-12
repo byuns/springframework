0264a49dd833c466826e754d3140c55cbfd4f97d
byuns <bsj0887@daum.net>
2022-04-12 11:39:53 +0900

    Controller-Request Parameter

--- src/main/java/com/mycompany/webapp/controller/HomeController.java
+++ src/main/java/com/mycompany/webapp/controller/HomeController.java
@@ -16,8 +16,8 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
-	@RequestMapping(value="/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	@RequestMapping("/")
+	public String home() {
 		logger.info("실행");
 		return "home";
 	}
